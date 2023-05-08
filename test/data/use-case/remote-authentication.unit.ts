462261ff96049ff6863fffe8443555244712267e
Breno Gonzaga <contato@gonzaga.dev.br>
2023-05-08 17:31:40 -0400

    test: ensure RemoteAuthentication throws InvalidCredentialsError if HttpPostClient returns 401

--- test/data/use-case/remote-authentication.unit.ts
+++ test/data/use-case/remote-authentication.unit.ts
@@ -2,6 +2,13 @@ import { AccountModel } from '../../domain/model/account.model';
 import { AuthModel } from '../../domain/model/auth.model';
 import { HttpPostClientSpy } from '../mock/mock-http-post-client';
 import { RemoteAuthentication } from '../../../src/data/use-case/remote-authentication';
+import { InvalidCredentialsError } from '../../../src/data/errors/invalid-credentials.error';
+import { HttpStatusCode } from 'axios';
+
+const account = {
+  email: 'valid_mail@mail.com',
+  password: 'valid_password',
+};
 
 type SutTypes = {
   sut: RemoteAuthentication;
@@ -16,21 +23,23 @@ const makeSut = (url = 'any_url'): SutTypes => {
 
 describe('RemoteAuthentication', () => {
   it('Should call HttpPostClient with correct URL', async () => {
-    const { sut, httpClient } = makeSut('any_url');
+    const { sut, httpClient } = makeSut();
     await sut.auth(null);
     expect(httpClient.url).toBe('any_url');
   });
 
   it('Should call HttpPostClient with correct Body', async () => {
-    const account = {
-      email: 'valid_mail@mail.com',
-      password: 'valid_password',
-    };
-    const { sut, httpClient } = makeSut('any_url');
+    const { sut, httpClient } = makeSut();
     await sut.auth(account);
-    expect(httpClient.body).toEqual({
-      email: 'valid_mail@mail.com',
-      password: 'valid_password',
-    });
+    expect(httpClient.body).toEqual(account);
+  });
+
+  it('Should throw InvalidCredentialsError if HttpPostClient returns 401', async () => {
+    const { sut, httpClient } = makeSut();
+    httpClient.response = {
+      statusCode: HttpStatusCode.Unauthorized,
+    };
+    const promise = sut.auth(account);
+    expect(promise).rejects.toThrow(new InvalidCredentialsError());
   });
 });
