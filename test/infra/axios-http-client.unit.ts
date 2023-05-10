be1be0d923baecb4a25f91371dc7d8b68fc6d4b7
Breno Gonzaga <contato@gonzaga.dev.br>
2023-05-09 22:10:25 -0400

    test: ensure AxiosHttpClient calls Axios with correct Body

--- test/infra/axios-http-client.unit.ts
+++ test/infra/axios-http-client.unit.ts
@@ -4,10 +4,26 @@ import axios from 'axios';
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
 
+const account = {
+  email: 'valid_mail@mail.com',
+  password: 'valid_password',
+};
+
+type SutTypes = {
+  sut: AxiosHttpClient;
+};
+
+const makeSut = (): SutTypes => {
+  const sut = new AxiosHttpClient();
+  return {
+    sut,
+  };
+};
+
 describe('AxiosHttpClient', () => {
-  it('Should call Axios with correct URL', async () => {
-    const sut = new AxiosHttpClient();
-    await sut.request({ url: 'any_url' });
-    expect(mockedAxios).toHaveBeenCalledWith('any_url');
+  it('Should call Axios with correct URL and Body', async () => {
+    const { sut } = makeSut();
+    await sut.request({ url: 'any_url', body: account });
+    expect(mockedAxios.post).toHaveBeenCalledWith('any_url', account);
   });
 });
