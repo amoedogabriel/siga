5fb1233aa4dc4a9373c648a127d04f4705e6ba51
Breno Gonzaga <contato@gonzaga.dev.br>
2023-05-09 22:18:46 -0400

    test: ensure AxiosHttpClient returns correct statusCode and Body

--- test/infra/axios-http-client.unit.ts
+++ test/infra/axios-http-client.unit.ts
@@ -3,6 +3,10 @@ import axios from 'axios';
 
 jest.mock('axios');
 const mockedAxios = axios as jest.Mocked<typeof axios>;
+mockedAxios.post.mockResolvedValue({
+  status: 201,
+  data: 'any_data',
+});
 
 const account = {
   email: 'valid_mail@mail.com',
@@ -25,5 +29,14 @@ describe('AxiosHttpClient', () => {
     const { sut } = makeSut();
     await sut.request({ url: 'any_url', body: account });
     expect(mockedAxios.post).toHaveBeenCalledWith('any_url', account);
+  });
+
+  it('Should return correct statusCode and Body', async () => {
+    const { sut } = makeSut();
+    const httpResponse = await sut.request({ url: 'any_url', body: account });
+    expect(httpResponse).toEqual({
+      statusCode: 201,
+      body: 'any_data',
+    });
   });
 });
